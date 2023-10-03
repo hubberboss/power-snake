732d9cc32e2dbc42812b7490f3b7d5c08bd3de79
Thomas Dohmke <thomas@dohmke.de>
2023-10-03 11:01:02 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Snake Game
+
+This is a simple implementation of the classic Snake game using HTML, CSS, and JavaScript.
+
+## How to Run
+
+To run the game, simply open the `index.html` file in your web browser.
+
+## How to Play
+
+The game starts immediately when you load the page. The snake moves in the direction of the arrow keys. The goal of the game is to eat as many apples as possible without colliding with the snake's own body.
+
+Each time the snake eats an apple, it grows longer and the game speed increases. The game ends when the snake collides with its own body.
+
+## Game Structure
+
+The game is structured as follows:
+
+- `index.html`: This is the main HTML file that contains the basic structure of the game, including a canvas element where the game is drawn.
+
+- `js/script.js`: This is the main JavaScript file for the game. It contains the game logic, including the Snake class and the game loop. The Snake class has properties like length and direction, and methods like move and eat. The game loop handles the game state and renders the game on the canvas.
+
+- `css/style.css`: This is the CSS file for the game. It contains the styles for the game, including the styles for the canvas and the snake.
+
+Enjoy the game!
---
+++ js/script.js
@@ -0,0 +1,137 @@
+// Game variables
+let canvas;
+let ctx;
+let gameLoop;
+const snake = [{ top: 250, left: 250 }];
+let direction = 'right';
+let apples = [];
+
+// Function to start the game
+function startGame() {
+  canvas = document.getElementById('gameCanvas');
+  ctx = canvas.getContext('2d');
+  
+  // Start the game loop
+  gameLoop = setInterval(game, 100);
+
+  // Generate 5 apples
+  for(let i = 0; i < 5; i++) {
+    generateApple();
+  }
+}
+
+// Function to generate a new apple
+function generateApple() {
+  apple = {
+    top: Math.floor(Math.random() * 20) * 25,
+    left: Math.floor(Math.random() * 20) * 25,
+  };
+  apples.push(apple); // Add the new apple to the array of apples
+}
+
+// Function to check if the snake has eaten an apple
+function checkAppleCollision() {
+  for (let i = 0; i < apples.length; i++) {
+    if (apples[i].top === snake[0].top && apples[i].left === snake[0].left) {
+      apples.splice(i, 1); // Remove the apple from the array
+      generateApple(); // Generate a new apple
+      return true;
+    }
+  }
+  return false;
+}
+
+// Function to check if the snake has collided with itself
+function checkSelfCollision() {
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].top === snake[0].top && snake[i].left === snake[0].left) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Function to update the game state
+function game() {
+  // Update the snake's position
+  switch (direction) {
+    case 'up':
+      snake.unshift({ top: (snake[0].top - 25 + 600) % 600, left: snake[0].left });
+      break;
+    case 'down':
+      snake.unshift({ top: (snake[0].top + 25) % 600, left: snake[0].left });
+      break;
+    case 'left':
+      snake.unshift({ top: snake[0].top, left: (snake[0].left - 25 + 600) % 600 });
+      break;
+    case 'right':
+      snake.unshift({ top: snake[0].top, left: (snake[0].left + 25) % 600 });
+      break;
+  }
+
+  // Check if the snake has eaten the apple
+  if (!checkAppleCollision()) {
+    snake.pop();
+  }
+
+  // Check if the snake has collided with itself
+  if (checkSelfCollision()) {
+    clearInterval(gameLoop);
+
+    // Draw game over screen
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.font = '25px Arial';
+    ctx.fillText('Game Over', 10, 30);
+    return;
+  }
+
+  // Draw the game
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'green';
+  snake.forEach(function (part) {
+    ctx.fillRect(part.left, part.top, 25, 25);
+  });
+
+  // Draw the apples
+  ctx.fillStyle = 'red';
+  apples.forEach(apple => ctx.fillRect(apple.left, apple.top, 25, 25));
+}
+
+// Event listener for key presses
+function changeDirection(key) {
+  let newDirection;
+  switch (key) {
+    case 'ArrowUp':
+      newDirection = 'up';
+      break;
+    case 'ArrowDown':
+      newDirection = 'down';
+      break;
+    case 'ArrowLeft':
+      newDirection = 'left';
+      break;
+    case 'ArrowRight':
+      newDirection = 'right';
+      break;
+  }
+
+  const isOpposite = (dir1, dir2) => {
+    return (dir1 === 'up' && dir2 === 'down') ||
+           (dir1 === 'down' && dir2 === 'up') ||
+           (dir1 === 'left' && dir2 === 'right') ||
+           (dir1 === 'right' && dir2 === 'left');
+  };
+
+  if (!isOpposite(direction, newDirection)) {
+    direction = newDirection;
+  }
+}
+
+window.addEventListener('keydown', function (e) {
+  changeDirection(e.key);
+  e.preventDefault();
+});
+
+// Start the game when the window loads
+window.addEventListener('load', startGame);
